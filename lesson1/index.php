b1f8e19bca3ce61f2eb0fda1d454c48772890c10
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-02 12:50:36 +0300

    done full homework

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,10 +1,11 @@
 <?php
+//tasks 1-3
 
 $my_name = 'Gashnikov Nikita';
 $my_age = 24;
 $foo = true;
 $float_number = 3.14;
-const MY_NULL = 555;
+const MY_NULL = null;
 
 echo "$my_name $my_age \n".MY_NULL." \n $foo $float_number";
 
@@ -15,3 +16,10 @@ var_dump($float_number / 4);
 var_dump($float_number + $my_age);
 var_dump($float_number ** 4);
 var_dump($my_age - $float_number);
+
+//task5
+var_dump($my_age >= $float_number);
+var_dump($float_number > $float_number);
+var_dump($my_name === 'Gashnikov Nikita');
+var_dump(MY_NULL == 0);
+var_dump(5 <=> 4);
