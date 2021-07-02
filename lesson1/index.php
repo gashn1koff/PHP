ea382250d9eaf63610f312c1958e8b3f4d3b1602
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-02 12:21:06 +0300

    first commit

---
+++ lesson1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$my_name = 'Gashnikov Nikita';
+$my_age = 24;
+$foo = true;
+$float_number = 3.14;
+const MY_NULL = 555;
+
+echo "$my_name $my_age \n".MY_NULL." \n $foo $float_number";
+//echo $my_age;
+//echo $foo;
+//echo $float_number;
+//echo MY_NULL;
