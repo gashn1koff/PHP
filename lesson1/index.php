ab4432bdd1f180902ca20dc8ec326152190b6242
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-02 12:37:51 +0300

    done task4

--- lesson1/index.php
+++ lesson1/index.php
@@ -7,3 +7,11 @@ $float_number = 3.14;
 const MY_NULL = 555;
 
 echo "$my_name $my_age \n".MY_NULL." \n $foo $float_number";
+
+//task4
+
+var_dump($my_age * 3);
+var_dump($float_number / 4);
+var_dump($float_number + $my_age);
+var_dump($float_number ** 4);
+var_dump($my_age - $float_number);
