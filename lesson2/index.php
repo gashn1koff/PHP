23948126e942da57425e1e7a9e6b64e571d06584
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-07 22:05:38 +0300

    'done task 1-2'

---
+++ lesson2/index.php
@@ -0,0 +1,13 @@
+<?php
+//task 1-2
+$arr = ['name' => 'Nikita',
+        'secondName' => 'Gashnikov',
+        1 => 'first',
+        'age' => 19,
+        'country' => 'Minsk',
+        'bool' => true,
+        'education' => 'BSUIR',
+        'dateOfBirth'=> '4.02.1997',
+        'customArray' => [1, 2, 3],
+        10 => 'last item'
+];
