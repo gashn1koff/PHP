2baf02bbabb0d7e0a7067f772a1fd41e91a89317
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-07 22:44:15 +0300

    done task3

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,18 +1,26 @@
 <?php
 //task 1-2
 $arr = ['name' => 'Nikita',
-        'secondName' => 'Gashnikov',
-        1 => 'first',
-        'age' => 19,
-        'country' => 'Minsk',
-        'bool' => true,
-        'education' => 'BSUIR',
-        'dateOfBirth'=> '4.02.1997',
-        'customArray' => [1, 2, 3],
-        10 => 'last item'
-
+    'secondName' => 'Gashnikov',
+    1 => 'first',
+    'age' => 19,
+    'country' => 'Minsk',
+    'bool' => true,
+    'education' => 'BSUIR',
+    'dateOfBirth'=> '4.02.1997',
+    'customArray' => [1, 2, 3],
+    10 => 'last item'
 ];
 
 foreach ($arr as $a){
-    print_r ($a);
+    print_r($a);
+    echo "<br>";
 };
+
+//task3
+
+$my_arr = [];
+for ($i = 0; $i < 20; $i++) {
+    $my_arr[] = rand(1, 20);
+}
+print_r($my_arr);
