2d3b4c1314f1c7c572c34cf39d7a88425792aeb2
Nikita Gashnikov <nikitagash@gmail.com>
2021-07-15 19:24:39 +0300

    done task 1

--- lesson3/index.php
+++ lesson3/index.php
@@ -1,1 +1,12 @@
 <?php
+// 1. Удалить из строки пробелы и определить, является ли она перевертышем
+
+$string = 'abc cba';
+$newStr = str_replace(' ', '', $string);
+
+if ($newStr == strrev($newStr)) {
+    echo 'Перевертыш';
+} else {
+    echo 'Не перевертыш';
+}
+
