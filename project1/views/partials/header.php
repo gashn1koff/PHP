003e4ecbe9d26039933368b8c0d9396e89b00398
Nikita Gashnikov <nikitagash@gmail.com>
2021-08-03 00:01:50 +0300

    lesson6

---
+++ project1/views/partials/header.php
@@ -0,0 +1,4 @@
+<header class="header-background">
+    <h1 class="header-title-text">Definition and features of the PHP language</h1>
+    <p class="header-title-subtext">Definition and features of the PHP language</p>
+</header>
